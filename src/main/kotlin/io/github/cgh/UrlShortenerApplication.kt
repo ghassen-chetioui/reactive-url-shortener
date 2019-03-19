f9b041332d8ceb5511ffa9189f4a5f16b2b97846
Ghassen Chetioui <g.chetioui@lectra.com>
2019-03-19 22:59:13 +0100

    refactor: use ServerResponse instead of ResponseEntity

--- src/main/kotlin/io/github/cgh/UrlShortenerApplication.kt
+++ src/main/kotlin/io/github/cgh/UrlShortenerApplication.kt
@@ -7,15 +7,13 @@ import org.springframework.boot.runApplication
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
 import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.data.redis.serializer.RedisSerializationContext
-import org.springframework.http.HttpHeaders.LOCATION
-import org.springframework.http.HttpStatus.MOVED_PERMANENTLY
-import org.springframework.http.ResponseEntity
-import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Mono
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import java.net.URI
 import java.nio.charset.StandardCharsets.UTF_8
 
 @RestController
@@ -24,17 +22,19 @@ class UrlShortener(connectionFactory: ReactiveRedisConnectionFactory) {
 
     private val redis = ReactiveRedisTemplate(connectionFactory, RedisSerializationContext.string()).opsForValue()
 
-    @PostMapping
-    fun save(req: ServerHttpRequest) = req.uri.toASCIIString().substringAfter("${req.uri.authority}/").let { url ->
+    @PostMapping("/{*url}")
+    fun save(req: ServerRequest) = req.pathVariable("url").substring(1).let { url ->
         if (UrlValidator(arrayOf("http", "https")).isValid(url)) {
-            murmur3_32().hashString(url, UTF_8).toString().let { id -> redis.set(id, url).map { ResponseEntity.ok("localhost:8080/$id") } }
-        } else Mono.just(ResponseEntity.badRequest().build())
+            murmur3_32().hashString(url, UTF_8).toString().let { id ->
+                redis.set(id, url).flatMap { ServerResponse.ok().syncBody("localhost:8080/$id") }
+            }
+        } else ServerResponse.badRequest().build()
     }
 
     @GetMapping("/{id}")
     fun get(@PathVariable id: String) = redis.get(id)
-            .map { ResponseEntity.status(MOVED_PERMANENTLY).header(LOCATION, it).build<Void>() }
-            .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()))
+            .flatMap { ServerResponse.permanentRedirect(URI(it)).build() }
+            .switchIfEmpty(ServerResponse.notFound().build())
 }
 
 fun main(args: Array<String>) {
