a7a5c8a87a1608fe2d45241711d6a97b8058721c
Ghassen Chetioui <g.chetioui@lectra.com>
2019-03-19 22:52:25 +0100

    fix beans definition

--- src/main/kotlin/io/github/cgh/UrlShortenerApplication.kt
+++ src/main/kotlin/io/github/cgh/UrlShortenerApplication.kt
@@ -3,15 +3,13 @@ package io.github.cgh
 import com.google.common.hash.Hashing.murmur3_32
 import org.apache.commons.validator.UrlValidator
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.data.redis.serializer.RedisSerializationContext
-import org.springframework.fu.kofu.configuration
 import org.springframework.fu.kofu.web.server
 import org.springframework.fu.kofu.webApplication
-import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Mono
 import java.net.URI
 import java.nio.charset.StandardCharsets.UTF_8
@@ -21,37 +19,31 @@ fun main() {
 }
 
 val app = webApplication {
-    configuration {
-        beans {
-            bean<UrlShortenerHandler>()
-            bean {
-                val handler = ref<UrlShortenerHandler>()
-                router {
-                    GET("/{id}", handler::get)
-                    POST("/", handler::save)
-                }
-            }
+    beans {
+        bean<UrlShortenerHandler>()
+        bean { LettuceConnectionFactory("localhost", 6379) }
+    }
+    server {
+        router {
+            val handler = ref<UrlShortenerHandler>()
+            GET("/{id}", handler::get)
+            POST("/{*url}", handler::save)
         }
     }
-    server {}
 }
 
 class UrlShortenerHandler(connectionFactory: ReactiveRedisConnectionFactory) {
     private val redis = ReactiveRedisTemplate(connectionFactory, RedisSerializationContext.string()).opsForValue()
 
     fun save(req: ServerRequest): Mono<ServerResponse> {
-        val httpRequest = req as ServerHttpRequest
-        val url = httpRequest.uri.toASCIIString().substringAfter("${httpRequest.uri.authority}/")
+        val url = req.pathVariable("url").substring(1)
         return if (UrlValidator(arrayOf("http", "https")).isValid(url)) {
             val id = murmur3_32().hashString(url, UTF_8).toString()
             redis.set(id, url).flatMap { ServerResponse.ok().syncBody("localhost:8080/$id") }
         } else ServerResponse.badRequest().build()
     }
 
-    fun get(req: ServerRequest): Mono<ServerResponse> {
-        return redis.get(req.pathVariable("id"))
-                .flatMap { ServerResponse.permanentRedirect(URI(it)).build() }
-                .switchIfEmpty(ServerResponse.notFound().build())
-    }
-
+    fun get(req: ServerRequest) = redis.get(req.pathVariable("id"))
+            .flatMap { ServerResponse.permanentRedirect(URI(it)).build() }
+            .switchIfEmpty(ServerResponse.notFound().build())
 }
