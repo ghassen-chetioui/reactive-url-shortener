2e43ce125981e47101ecb8a56dcddb904707a2e1
Ghassen Chetioui <g.chetioui@lectra.com>
2019-03-19 18:32:27 +0100

    functionnal beans declaration

--- src/main/kotlin/io/github/cgh/UrlShortenerApplication.kt
+++ src/main/kotlin/io/github/cgh/UrlShortenerApplication.kt
@@ -2,41 +2,56 @@ package io.github.cgh
 
 import com.google.common.hash.Hashing.murmur3_32
 import org.apache.commons.validator.UrlValidator
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
 import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.data.redis.serializer.RedisSerializationContext
-import org.springframework.http.HttpHeaders.LOCATION
-import org.springframework.http.HttpStatus.MOVED_PERMANENTLY
-import org.springframework.http.ResponseEntity
+import org.springframework.fu.kofu.configuration
+import org.springframework.fu.kofu.web.server
+import org.springframework.fu.kofu.webApplication
 import org.springframework.http.server.reactive.ServerHttpRequest
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Mono
+import java.net.URI
 import java.nio.charset.StandardCharsets.UTF_8
 
-@RestController
-@SpringBootApplication
-class UrlShortener(connectionFactory: ReactiveRedisConnectionFactory) {
-
-    private val redis = ReactiveRedisTemplate(connectionFactory, RedisSerializationContext.string()).opsForValue()
-
-    @PostMapping
-    fun save(req: ServerHttpRequest) = req.uri.toASCIIString().substringAfter("${req.uri.authority}/").let { url ->
-        if (UrlValidator(arrayOf("http", "https")).isValid(url)) {
-            murmur3_32().hashString(url, UTF_8).toString().let { id -> redis.set(id, url).map { ResponseEntity.ok("localhost:8080/$id") } }
-        } else Mono.just(ResponseEntity.badRequest().build())
-    }
-
-    @GetMapping("/{id}")
-    fun get(@PathVariable id: String) = redis.get(id)
-            .map { ResponseEntity.status(MOVED_PERMANENTLY).header(LOCATION, it).build<Void>() }
-            .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()))
+fun main() {
+    app.run()
 }
 
-fun main(args: Array<String>) {
-    runApplication<UrlShortener>(*args)
+val app = webApplication {
+    configuration {
+        beans {
+            bean<UrlShortenerHandler>()
+            bean {
+                val handler = ref<UrlShortenerHandler>()
+                router {
+                    GET("/{id}", handler::get)
+                    POST("/", handler::save)
+                }
+            }
+        }
+    }
+    server {}
+}
+
+class UrlShortenerHandler(connectionFactory: ReactiveRedisConnectionFactory) {
+    private val redis = ReactiveRedisTemplate(connectionFactory, RedisSerializationContext.string()).opsForValue()
+
+    fun save(req: ServerRequest): Mono<ServerResponse> {
+        val httpRequest = req as ServerHttpRequest
+        val url = httpRequest.uri.toASCIIString().substringAfter("${httpRequest.uri.authority}/")
+        return if (UrlValidator(arrayOf("http", "https")).isValid(url)) {
+            val id = murmur3_32().hashString(url, UTF_8).toString()
+            redis.set(id, url).flatMap { ServerResponse.ok().syncBody("localhost:8080/$id") }
+        } else ServerResponse.badRequest().build()
+    }
+
+    fun get(req: ServerRequest): Mono<ServerResponse> {
+        return redis.get(req.pathVariable("id"))
+                .flatMap { ServerResponse.permanentRedirect(URI(it)).build() }
+                .switchIfEmpty(ServerResponse.notFound().build())
+    }
+
 }
